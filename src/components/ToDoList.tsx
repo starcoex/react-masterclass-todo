9358abcdccb7805320b6191163709a841b705447
starcoex <69073390+starcoex@users.noreply.github.com>
2023-02-27 12:35:27 +0900

    react-todo form-hoom part4

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { toDoSelector, toDoState } from '../recoil/atoms';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, toDoSelector, toDoState } from '../recoil/atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 export default function ToDoList() {
-  const toDos = useRecoilValue(toDoState)
-  const [toDo, doing, done] = useRecoilValue(toDoSelector)
-  console.log(toDo, doing, done)
+  // const toDos = useRecoilValue(toDoState)
+  const [category, setCategory] = useRecoilState(categoryState)
+  const toDos = useRecoilValue(toDoSelector)
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    console.log(event)
+    const { currentTarget: { value } } = event
+    console.log(value)
+    setCategory(value)
+  }
   return (
     <div>
       <h1>To Dos</h1>
@@ -16,17 +23,31 @@ export default function ToDoList() {
       {/* {toDos.map((currnet) => <ToDo id={currnet.id} text={currnet.text} category={currnet.category} />)} */}
       {/* {toDos.map((toDo) => <ToDo key={toDo.id} {...toDo} />)} */}
       <h2>To Do</h2>
-      <ul>
+      <select onInput={onInput} value={category}>
+        <option value='TO-DO'>toDo</option>
+        <option value='DOING'>Doing</option>
+        <option value='DONE'>Done</option>
+      </select>
+      {toDos?.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
+      {/* {category === "TO-DO" && toDo.map((aToDo) => <ToDo key={aToDo.id}{...aToDo} />)}
+      {category === "DOING" && doing.map((aToDo) => <ToDo key={aToDo.id}{...aToDo} />)}
+      {category === "DONE" && done.map((aToDo) => <ToDo key={aToDo.id}{...aToDo} />)} */}
+      {/* <ul>
         {toDo.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
       </ul>
+      <hr />
       <h2>Doing</h2>
       <ul>
         {doing.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
       </ul>
+      <hr />
+
       <h2>Done</h2>
       <ul>
         {done.map((toDo) => <ToDo key={toDo.id} {...toDo} />)}
       </ul>
+      <hr /> */}
+
     </div >
   );
 }
--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -10,15 +10,22 @@ export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
 });
+export const categoryState = atom({
+  key: 'category',
+  default: 'TO-DO',
+});
 
 export const toDoSelector = selector({
   key: 'toDoSelector',
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === 'TO-DO'),
-      toDos.filter((toDo) => toDo.category === 'DOING'),
-      toDos.filter((toDo) => toDo.category === 'DOING'),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+    // if (category === 'TO-DO')
+    //   return toDos.filter((toDo) => toDo.category === 'TO-DO');
+    // if (category === 'DOING')
+    //   return toDos.filter((toDo) => toDo.category === 'DOING');
+    // if (category === 'DONE')
+    //   return toDos.filter((toDo) => toDo.category === 'DONE');
   },
 });
